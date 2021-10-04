73d92200a4a5431e6f01517055f4f08423c71d1f
Emre Kardaşlar <emre.kardaslar@gmail.com>
2021-10-04 20:16:22 +0300

    Recipe info pass between components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>(); 
   recipes: Recipe[] = [
     new Recipe(
       'Pizza',
@@ -17,4 +18,10 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    console.log('recipe was selected', recipe);
+    this.recipeWasSelected.emit(recipe);
+  }
+
 }
