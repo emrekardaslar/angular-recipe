7c013f2b1621f5be1043fc9d554550f282d71246
Emre Kardaşlar <emre.kardaslar@gmail.com>
2021-10-12 20:07:38 +0300

    add recipe ingredients to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,9 +10,15 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
   
-  constructor() { }
+  constructor(private shoppingService: ShoppingListService) { }
 
   ngOnInit(): void {
+  }
+
+  onAddToShoppingList() {
+    this.recipe.ingredients.forEach(ingredient => {
+      this.shoppingService.addIngredient(ingredient);
+    });
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -30,6 +30,4 @@ export class RecipeService {
     getRecipes() {
         return this.recipes.slice();
     }
-
-    
 }
