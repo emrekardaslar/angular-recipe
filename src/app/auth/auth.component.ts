18cadab3dfb9296cd397d836e3ac5c9d32ddb7ce
Emre Kardaşlar <emre.kardaslar@gmail.com>
2021-10-24 12:45:10 +0300

    add signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,39 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
-    selector: "app-auth",
-    templateUrl: "./auth.component.html"
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  isLoginMode = true;
+  isLoading = false;
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+      //...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      ),
+        form.reset();
+    }
+  }
+}
